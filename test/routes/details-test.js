59e366c00bb9a3bfcc90a9b8073bb699055b2185
Yan Liu <278899085@qq.com>
2018-11-02 19:40:41 +0000

    add details-get-tested

--- test/routes/details-test.js
+++ test/routes/details-test.js
@@ -6,5 +6,22 @@ chai.use(require("chai-things"));
 chai.use(chaiHttp);
 let _ = require("lodash" );
 describe("Details", function () {
-
+    describe("GET /details", () => {
+        it("should return the details which combine with sender,receiver,good collection", function (done) {
+            chai.request(server)
+                .get("/details")
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(res.body.length).to.equal(4);
+                    let result = _.map(res.body, (goods) => {
+                        return {_id: goods._id};
+                    });
+                    expect(result).to.include({_id: 10001});
+                    expect(result).to.include({_id: 10002});
+                    expect(result).to.include({_id: 10003});
+                    expect(result).to.include({_id: 10004});
+                    done();
+                });
+        });
+    });
 });
